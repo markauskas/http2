760bd42bff8b3f75fda1217e7ca284854c4e9e0e
Tomas Markauskas <tomas@markauskas.lt>
2016-04-07 23:32:25 +0200

    Fix specs

--- spec/http2/stream_spec.cr
+++ spec/http2/stream_spec.cr
@@ -2,10 +2,9 @@ require "../spec_helper"
 
 describe HTTP2::Stream do
   context "in the IDLE state" do
-    stream = HTTP2::Stream.new(1_u32, HTTP2::Stream::State::IDLE)
-
     context "after receiving a HEADERS frame" do
       it "transitions to the OPEN state" do
+        stream = HTTP2::Stream.new(1_u32, HTTP2::Stream::State::IDLE)
         stream.receive(HTTP2::Frame.new(HTTP2::Frame::Type::Headers, stream.id, HTTP2::Frame::Flags::None, "".to_slice))
         stream.state.should eq HTTP2::Stream::State::OPEN
       end
@@ -13,6 +12,7 @@ describe HTTP2::Stream do
 
     context "after receiving a HEADERS frame w/ END_STREAM flag" do
       it "transitions to the HALF_CLOSED_REMOTE state" do
+        stream = HTTP2::Stream.new(1_u32, HTTP2::Stream::State::IDLE)
         stream.receive(HTTP2::Frame.new(HTTP2::Frame::Type::Headers, stream.id, HTTP2::Frame::Flags::EndStream, "".to_slice))
         stream.state.should eq HTTP2::Stream::State::HALF_CLOSED_REMOTE
       end
